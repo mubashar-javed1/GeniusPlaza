c88a974a30a01b5538592cb7f26b294d72921eb6
mubashar-javed1 <mubasharjaved10@gmail.com>
2019-07-16 20:59:29 -0500

    stated from scratch, created empty acitivity and add necessary dependencies

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+A
+gradle.properties,2\a\2afbb999f001938c88fa43fc2ef52abf0f8213e4
+e
+5app/src/main/java/com/mobi/users/ui/MainActivity.java,d\4\d48e76cc0c268ef58c1e92ffc256414230fee924
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
---
+++ app/src/main/java/com/mobi/geniusplaza/ui/mainactivity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mobi.geniusplaza.ui.mainactivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mobi.geniusplaza.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
